7c22011721808619118b6d14e80bec4c1c4a9daf
Piotr <piotrmartyla@gmail.com>
2016-08-24 17:16:54 +0200

    First commit

---
+++ app/src/main/java/com/example/piotr/testongithub/MainActivity.java
@@ -0,0 +1,19 @@
+package com.example.piotr.testongithub;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void onChange(View v){
+        Log.i("DEBUG MSG", "onChange method");
+    }
+}
